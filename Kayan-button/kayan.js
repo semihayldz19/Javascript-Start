eeb677617b34b709d86207e78b49f7ce596cebb8
semiha yıldız karaderili <semihakaraderili@gmail.com>
2023-02-27 20:07:39 +0300

    kayan buton eklendi

---
+++ Kayan-button/kayan.js
@@ -0,0 +1,31 @@
+let bas=document.getElementById("bas");
+
+
+var i=0;
+while(i<=5)
+{
+   
+
+bas.addEventListener("click",function()
+{
+
+    bas.classList.add("kaydır_sağ");
+
+})
+
+
+bas.addEventListener("click",function(){
+
+    bas.classList.add("kaydır_sol");
+
+
+})
+
+
+i++;
+
+}
+
+
+
+
